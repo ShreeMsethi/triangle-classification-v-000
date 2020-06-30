ee4139dc4c596090aeef0c1fc6a752184ee2e40c
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-06-30 00:36:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
   # write code here
+  
+  def initialize(s_1, s_2, s_3)
+    @s_1 = s_1
+    @s_2 = s_2
+    @s_3 = s_3
+  end
+  
+  def kind(type)
+    type
+    raise TriangleError
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
